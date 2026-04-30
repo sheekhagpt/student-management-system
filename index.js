ba321929070f26cfe77ef31d3ebd7609e6eb398b
sheekhagpt <sheekha24@navgurukul.org>
2026-04-30 12:38:39 +0530

    Fix /students route

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");   // 👈 ये add करना है
+const cors = require("cors");
 const connectDB = require("./config/db");
+const studentRoutes = require("./routes/studentRoutes");
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());   // 👈 ये add करना है (IMPORTANT)
+app.use(cors());
 app.use(express.json());
 
-app.use("/students", require("./routes/studentRoutes"));
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.use("/students", studentRoutes);
 
 const PORT = process.env.PORT || 5000;
 
